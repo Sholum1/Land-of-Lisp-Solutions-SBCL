e7e9f499462a7485c615a83eb39aa5e50d27b0ac
Wallysson Oliveira <wallyssonryu@gmail.com>
2024-10-07 14:11:29 -0300

    Remove git trash

--- Web Socket/server.lisp
+++ Web Socket/server.lisp
@@ -1,10 +1,6 @@
 (require 'usocket)
 
-<<<<<<< HEAD
-(defparameter my-socket (usocket:socket-listen #(127 0 0 1) 4321 :reuse-adress t))
-=======
 (defparameter my-socket (usocket:socket-listen #(127 0 0 1) 4321 :reuse-address t))
->>>>>>> e11d16f (Lazy)
 (defparameter my-stream (usocket:socket-stream (usocket:socket-accept my-socket)))
 
 (read my-stream)
